8f1feed655678ce583ec492f9d8f08e6028f392b
Jack <jackc2849@gmail.com>
2019-12-26 00:32:43 -0500

    Completed tutorial on react-router section(part7a)

---
+++ react-router/src/components/Notes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notes = () => {
+    return (
+        <div>
+            <h2>{note.content}</h2>
+            <div>{note.user}</div>
+            <div><strong>{note.important ? 'important' : ''}</strong></div>
+        </div>
+    )
+}
+
+export default Notes;
---
+++ react-router/src/components/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div>
+            <h2>You're on the Users page</h2>
+        </div>
+    )
+}
+
+export default Users;
