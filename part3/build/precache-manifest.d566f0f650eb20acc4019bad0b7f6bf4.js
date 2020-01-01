0f2e482977d95bf2777cb070aecee5160b9389a0
Jack <jackc2849@gmail.com>
2020-01-01 17:35:56 -0500

    production build test

---
+++ part3/build/precache-manifest.d566f0f650eb20acc4019bad0b7f6bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356b68e69b3530954329aba9da4420bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8129cdd52df878074b80",
+    "url": "/static/css/main.6a5a3e24.chunk.css"
+  },
+  {
+    "revision": "020b8c824c10e8ff234c",
+    "url": "/static/js/2.e37b516f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e37b516f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8129cdd52df878074b80",
+    "url": "/static/js/main.68a55dfd.chunk.js"
+  },
+  {
+    "revision": "1ecde0206920711fe034",
+    "url": "/static/js/runtime-main.d2947fe0.js"
+  }
+]);
